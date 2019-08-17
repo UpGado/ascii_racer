c99a2b6a7127e7cdc5bed402dd752d2addc10412
Ahmed Gado <ahmedehabg@gmail.com>
2019-08-17 13:19:30 -0400

    Fixed square screen size

--- asciiracer/config.py
+++ asciiracer/config.py
@@ -4,7 +4,7 @@ import os
 # Definitions:
 # - [X]_STICKY_TIME: amount of time a key press of action [X]
 #                    sticks in the game
-GAME_SIZE = tuple(int(i) for i in os.popen('stty size', 'r').read().split())
+GAME_SIZE = (100, 100)
 FPS = 60
 
 # Car movement
