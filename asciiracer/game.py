f350ad1554e7acc1c81fa3e6bbc883a16f2a8417
Ahmed Gado <ahmedehabg@gmail.com>
2019-08-17 11:21:09 -0400

    Move game into a class

--- asciiracer/game.py
+++ asciiracer/game.py
@@ -9,47 +9,46 @@ from .config import GAME_SIZE, FPS, BASE_SPEED
 from .misc import limit_fps
 
 
-SCENE = [draw_statusbar, draw_hud, draw_horizon, draw_tracks,
-         draw_debris, draw_car, draw_money, draw_background]
-state = {'frames': 0,
-         'time': 0.0,  # seconds
-         'speed': BASE_SPEED,  # coord per frame
-         'car': None,
-         'car_x': 0,  # range -1:1
-         'car_steer_tuple': None,
-         'car_speed_tuple': None,
-         'debris': [],  # debris objects drawn in scene
-         'money': [],  # money objects drawn in scene
-         'score': 0,
-         'pdb': False}  # for testing
+class AsciiRacer(object):
+    def __init__(self):
+        self.SCENE = [draw_statusbar, draw_hud, draw_horizon, draw_tracks,
+                      draw_debris, draw_car, draw_money, draw_background]
+        self.state = {'frames': 0,
+                      'time': 0.0,  # seconds
+                      'speed': BASE_SPEED,  # coord per frame
+                      'car': None,
+                      'car_x': 0,  # range -1:1
+                      'car_steer_tuple': None,
+                      'car_speed_tuple': None,
+                      'debris': [],  # debris objects drawn in scene
+                      'money': [],  # money objects drawn in scene
+                      'score': 0,
+                      'pdb': False}  # for testing
 
+    @limit_fps(fps=FPS)
+    def draw_scene(self, screen):
+        for draw_element in reversed(self.SCENE):
+            draw_element(screen, self.state)
+        screen.refresh()
 
-@limit_fps(fps=FPS)
-def draw_scene(screen):
-    for draw_element in reversed(SCENE):
-        draw_element(screen, state)
-    screen.refresh()
+    def main(self, screen):
+        screen.resize(*GAME_SIZE)
+        screen.nodelay(True)
+        environment.init(screen)
+        hud.init(screen)
+        while True:
+            self.draw_scene(screen)
+            key = screen.getch()
+            if key == ord('q'):
+                break
+            elif key == ord('p'):
+                self.state['pdb'] = True
+            else:
+                update_state(key, self.state)
+            self.state['frames'] += 1
+            self.state['time'] += 1/FPS
+        screen.clear()
+        screen.getkey()
 
-
-def main(screen):
-    screen.resize(*GAME_SIZE)
-    screen.nodelay(True)
-    environment.init(screen)
-    hud.init(screen)
-    while True:
-        draw_scene(screen)
-        key = screen.getch()
-        if key == ord('q'):
-            break
-        elif key == ord('p'):
-            state['pdb'] = True
-        else:
-            update_state(key, state)
-        state['frames'] += 1
-        state['time'] += 1/FPS
-    screen.clear()
-    screen.getkey()
-
-
-def run():
-    curses.wrapper(main)
+    def run(self):
+        curses.wrapper(self.main)
