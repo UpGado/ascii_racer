f524e0e1c6f01d3494823649ac0895cd9c79f849
Ahmed Gado <ahmedehabg@gmail.com>
2019-08-17 12:57:26 -0400

    Example of playing as an agent

--- asciiracer/__main__.py
+++ asciiracer/__main__.py
@@ -1,9 +1,28 @@
 from .game import AsciiRacer
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 def main():
+    # Example for how to use asciiracer class as an agent
     asciiracer = AsciiRacer()
-    asciiracer.run()
+    matrices = []
+    for x in range(100):
+        score, matrix = asciiracer.step(ord('w'))
+        matrices.append(matrix)
+    asciiracer.step(ord('q'))
+    animate(matrices)
+
+
+def animate(movie):
+    fig = plt.figure()
+    ims = []
+    for image in movie:
+        im = plt.imshow(image, animated=True)
+        ims.append([im])
+    _ = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                  repeat_delay=1000)
+    plt.show()
 
 
 main()
